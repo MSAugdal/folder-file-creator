91b8f24834ddfbe9e64800213ecc9c42d9521f7d
Mathias Schjølberg Augdal <mathias.s.augdal@gmail.com>
2021-02-08 23:51:01 +0100

    Update file-folder.py

--- file-folder.py
+++ file-folder.py
@@ -1,1 +1,15 @@
+from pathlib import Path
 
+
+def folder_file_creator(folder_name, file_name):
+    path = r"F:\OneDrive\Skrivebord"
+
+    directory = Path(path) / folder_name
+    directory.mkdir()
+
+    complete_file = Path.joinpath(directory, file_name+".py")
+    with open(complete_file, "w") as file:
+        file.writelines("#auto created file")
+
+
+folder_file_creator(folder_name="your_folder_name", file_name="your_file_name")
